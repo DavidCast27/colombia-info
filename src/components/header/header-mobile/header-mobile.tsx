a942eb04ef2a8016c7b9c63529cba7ee52669ecb
davilapa <diegoavilapava@gmail.com>
2024-01-28 11:52:57 -0500

    feat: adding new styles to show the cards

--- src/components/header/header-mobile/header-mobile.tsx
+++ src/components/header/header-mobile/header-mobile.tsx
@@ -1,9 +1,9 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
+'use client';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
 
-import { Nav } from "@/components/nav/nav";
+import { Nav } from '@/components/nav/nav';
 import {
   Drawer,
   DrawerClose,
@@ -11,55 +11,55 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
-} from "@/components/ui/drawer";
-import { Close } from "@/components/ui/icons/close";
-import { Menu } from "@/components/ui/icons/menu";
-import { Typography } from "@/components/ui/typography";
+} from '@/components/ui/drawer';
+import { Close } from '@/components/ui/icons/close';
+import { Menu } from '@/components/ui/icons/menu';
+import { Typography } from '@/components/ui/typography';
 
 export function HeaderMobile() {
   const [open, setOpen] = useState(false);
 
   return (
-    <header className="flex justify-between md:hidden p-4">
-      <Link href="/" className="flex items-center justify-center">
+    <header className='flex justify-between md:hidden p-4'>
+      <Link href='/' className='flex items-center justify-center'>
         <Image
-          src="/assets/flag.png"
-          alt="colombia Logo"
+          src='/assets/flag.png'
+          alt='colombia Logo'
           width={50}
           height={50}
         />
         <Typography
-          variant="span"
-          className="self-center text-xl font-semibold whitespace-nowrap pl-2"
+          variant='span'
+          className='self-center text-xl font-semibold whitespace-nowrap pl-2'
         >
           Info COLOMBIA
         </Typography>
       </Link>
-      <Drawer direction="right" open={open} onOpenChange={setOpen}>
+      <Drawer direction='right' open={open} onOpenChange={setOpen}>
         <DrawerTrigger>
           <Menu />
         </DrawerTrigger>
-        <DrawerContent className="h-full">
+        <DrawerContent className='h-full'>
           <DrawerHeader>
             <DrawerClose asChild>
               <Close
-                className="absolute right-3 top-3"
+                className='absolute right-3 top-3'
                 // onClick={() => {
                 //   setOpen(false);
                 // }}
               />
             </DrawerClose>
             <DrawerTitle>
-              <Link href="/" className="flex items-center justify-center mb-20">
+              <Link href='/' className='flex items-center justify-center mb-20'>
                 <Image
-                  src="/assets/flag.png"
-                  alt="colombia Logo"
+                  src='/assets/flag.png'
+                  alt='colombia Logo'
                   width={50}
                   height={50}
                 />
                 <Typography
-                  variant="span"
-                  className="self-center text-xl font-semibold whitespace-nowrap pl-2"
+                  variant='span'
+                  className='self-center text-xl font-semibold whitespace-nowrap pl-2'
                 >
                   Info COLOMBIA
                 </Typography>
--- src/components/ui/icons/close.tsx
+++ src/components/ui/icons/close.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
 export function Close({
   className,
@@ -11,21 +11,21 @@ export function Close({
 }): React.ReactElement {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
+      xmlns='http://www.w3.org/2000/svg'
       className={cn(className)}
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      strokeWidth="2"
-      stroke="currentColor"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      strokeWidth='2'
+      stroke='currentColor'
+      fill='none'
+      strokeLinecap='round'
+      strokeLinejoin='round'
       onClick={onClick}
     >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M18 6l-12 12" />
-      <path d="M6 6l12 12" />
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M18 6l-12 12' />
+      <path d='M6 6l12 12' />
     </svg>
   );
 }
